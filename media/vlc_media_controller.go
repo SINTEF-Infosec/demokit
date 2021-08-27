f03c2cec1e2a3103ffdca79f9426806f12aae0b3
Guillaume Bour <Guillaume.Bour@sintef.no>
2021-08-27 13:14:13 +0200

    Improve media handling in media controller.

--- media/vlc_media_controller.go
+++ media/vlc_media_controller.go
@@ -6,13 +6,11 @@ import (
 	"fmt"
 	"github.com/adrg/libvlc-go/v3"
 	log "github.com/sirupsen/logrus"
-	"time"
 )
 
 type VLCMediaController struct {
 	logger       *log.Entry
 	player       *vlc.Player
-	currentMedia *vlc.Media
 	eventManager *vlc.EventManager
 
 	onMediaStartedCallback MediaEventCallback
@@ -81,91 +79,63 @@ func (mc *VLCMediaController) IsAvailable() bool {
 }
 
 func (mc *VLCMediaController) LoadMediaFromPath(path string) error {
-	if mc.currentMedia != nil {
-		mc.logger.Debugf("releasing previous media")
-		if err := mc.currentMedia.Release(); err != nil {
-			mc.logger.Errorf("could not release previous media: %v", err)
-		}
-		mc.logger.Debugf("previous media released")
+	mc.logger.Debugf("loading media from path: %s", path)
+	if err := mc.releaseCurrentMedia(); err != nil {
+		mc.logger.Errorf("could not release previous media: %v", err)
 	}
 
 	mc.logger.Debugf("loading media from file: %s", path)
-
-	// 2 seconds timeout
-	c1 := make(chan bool, 1)
-	c2 := make(chan bool, 1)
-
-	go func() {
-		time.Sleep(2 * time.Second)
-		c1 <- true
-	}()
-
-	var media *vlc.Media
-	go func() {
-		var err error
-		media, err = mc.player.LoadMediaFromPath(path)
-		if err != nil {
-			mc.logger.Fatalf("could not load media from file: %v", err)
-		}
-		c2 <- true
-	}()
-
-	select {
-		case <-c1:
-			mc.logger.Errorf("LoadMediaFromPath timed out...")
-			return fmt.Errorf("LoadMediaFromPath timed out")
-		case <-c2:
-			mc.logger.Debugf("LoadMediaFromPath succeeded")
+	_, err := mc.player.LoadMediaFromPath(path)
+	if err != nil {
+		mc.logger.Fatalf("could not load media from file: %v", err)
+		return err
 	}
 
-	mc.currentMedia = media
 	mc.logger.Debugf("media loaded successfully", path)
 	return nil
 }
 
 func (mc *VLCMediaController) LoadMediaFromURL(url string) error {
 	mc.logger.Debugf("loading media from url: %s", url)
-	if mc.currentMedia != nil {
-		if err := mc.currentMedia.Release(); err != nil {
-			mc.logger.Errorf("could not release previous media: %v", err)
-		}
+	if err := mc.releaseCurrentMedia(); err != nil {
+		mc.logger.Errorf("could not release previous media: %v", err)
 	}
 
-	media, err := mc.player.LoadMediaFromURL(url)
+	_, err := mc.player.LoadMediaFromURL(url)
 	if err != nil {
 		mc.logger.Fatalf("could not load media from url: %v", err)
 		return err
 	}
 
-	mc.currentMedia = media
 	return nil
 }
 
 func (mc *VLCMediaController) Play() error {
-	if mc.currentMedia != nil {
+	if mc.isMediaAvailable() {
 		return mc.player.Play()
 	}
 	return fmt.Errorf("no media loaded")
 }
 
 func (mc *VLCMediaController) Pause() error {
-	if mc.currentMedia != nil {
+	if mc.isMediaAvailable() {
 		return mc.player.TogglePause()
 	}
 	return fmt.Errorf("no media loaded")
 }
 
 func (mc *VLCMediaController) Mute() error {
-	if mc.currentMedia != nil {
+	if mc.isMediaAvailable() {
 		return mc.player.ToggleMute()
 	}
 	return fmt.Errorf("no media loaded")
 }
 
 func (mc *VLCMediaController) Stop() error {
-	if mc.currentMedia != nil {
-		mc.currentMedia.Release()
-		mc.currentMedia = nil
+	if mc.isMediaAvailable() {
+		if err := mc.releaseCurrentMedia(); err != nil {
+			return err
+		}
 		return mc.player.Stop()
 	}
 	return fmt.Errorf("no media loaded")
@@ -181,4 +151,33 @@ func (mc *VLCMediaController) SetOnMediaPausedCallback(cb MediaEventCallback) {
 
 func (mc *VLCMediaController) SetOnMediaEndedCallback(cb MediaEventCallback) {
 	mc.onMediaEndedCallback = cb
+}
+
+func (mc *VLCMediaController) isMediaAvailable() bool {
+	if mc.player != nil {
+		currentMedia, err := mc.player.Media()
+		if err != nil {
+			mc.logger.Errorf("could not get current media: %v", err)
+			return false
+		}
+		return currentMedia != nil
+	}
+	return false
+}
+
+func (mc *VLCMediaController) releaseCurrentMedia() error {
+	if mc.player != nil {
+		currentMedia, err := mc.player.Media()
+		if err != nil {
+			mc.logger.Errorf("could not get current media: %v", err)
+			return fmt.Errorf("could not get current media: %v", err)
+		}
+
+		err = currentMedia.Release()
+		if err != nil {
+			return fmt.Errorf("could not release current media: %v", err)
+		}
+		return nil
+	}
+	return fmt.Errorf("player is nil")
 }

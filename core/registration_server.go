0400a4bdd63a73952cb3aaaeff6eef1477386e06
Guillaume Bour <Guillaume.Bour@sintef.no>
2021-11-16 14:59:01 +0100

    Add nodes infos fetching capabilities.

--- core/registration_server.go
+++ core/registration_server.go
@@ -4,9 +4,16 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
+
+type RegisteredNode struct {
+	NodeInfo        NodeInfo   `json:"info"`
+	NodeStatus      NodeStatus `json:"status"`
+	failUpdateCount int
+}
 
 type RegistrationServer struct {
 	Addr string
@@ -46,4 +53,38 @@ func (rs *RegistrationServer) RegisterNode(node *Node) error {
 	}
 
 	return nil
+}
+
+func (rs *RegistrationServer) FetchNodesInfo() ([]RegisteredNode, error) {
+	client := http.Client{
+		Timeout: 2 * time.Second,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s/nodes", rs.Addr), nil)
+	if err != nil {
+		return nil, fmt.Errorf("could not create request: %v", err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("could not do request: %v", err)
+	}
+
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("incorrect response code, expected 200, received %d", res.StatusCode)
+	}
+
+	// Getting the results
+	// we expect a list of node info
+	infos := make([]RegisteredNode, 0)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(body, &infos); err != nil {
+		return nil, err
+	}
+
+	return infos, nil
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,21 @@ module github.com/SINTEF-Infosec/demokit
 go 1.16
 
 require (
-	github.com/DataDog/go-python3 v0.0.0-20210805105248-03d93fb21b67
-	github.com/adrg/libvlc-go/v3 v3.1.4
+	github.com/DataDog/go-python3 v0.0.0-20211102160307-40adc605f1fe
+	github.com/adrg/libvlc-go/v3 v3.1.5
 	github.com/gin-gonic/gin v1.7.4
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/goombaio/namegenerator v0.0.0-20181006234301-989e774b106e
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/sirupsen/logrus v1.8.1
 	github.com/streadway/amqp v1.0.0
+	github.com/ugorji/go v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20211115234514-b4de73f9ece8 // indirect
+	golang.org/x/sys v0.0.0-20211116061358-0a5406a5449c // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
